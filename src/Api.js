6d6bf0825038ed28a86d79ec96fabed8ed300bea
Wanyan Zhang <wanyanzhang@WanyannoMacBook-Air.local>
2022-10-18 09:11:16 +0900

    urlのsearchワードを変更

--- src/Api.js
+++ src/Api.js
@@ -6,7 +6,7 @@ class App extends Component {
     }
 
     componentDidMount() {
-        const url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*'
+        const url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=Dancing&format=json&origin=*'
     
         fetch(url)
             .then((result) => result.json())
